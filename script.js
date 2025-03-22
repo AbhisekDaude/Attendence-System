85059583dac600d910f24f20a1f9f333fd2c0ae0
Abhisek Daude <abhisekdaude1@gmail.com>
2025-03-23 00:32:03 +0545

    last change

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let inp = document.querySelector(".input");
 let btn = document.querySelector("button");
 let save=document.querySelector(".save");
+let remove=document.querySelector(".remove");
+
 
 
 
@@ -20,10 +22,13 @@ inp.addEventListener("keypress", function (event) {
  function attendance () {
     let input = inp.value.trim(); // input ma hanya ko value linya vayo yolay
     if (input === "") return;// yeslay chai input box khali xa ki vanyar check garnya vayo
-
+  
     let list = document.querySelector(".studentlist"); //ul list lai list vanya variable vitra store garyau
+ 
     let li = document.createElement("li");// li html create garyau
     let span = document.createElement("span");//span html create garyau
+    let btn1=document.createElement("button");
+
     
     const d = new Date();// we store the date in d variable 
     let date = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`; // Format date as DD/MM/YYYY
@@ -38,7 +43,9 @@ inp.addEventListener("keypress", function (event) {
     deleteBtn.style.border = "none";
     deleteBtn.style.padding = "5px 8px";
     deleteBtn.style.cursor = "pointer";
-    deleteBtn.style.borderRadius = "5px";
+    deleteBtn.style.borderRadius = "50px";
+    
+
 
     deleteBtn.addEventListener("click", function () {
         li.remove(); // Remove the list item when delete button is clicked
@@ -48,9 +55,14 @@ inp.addEventListener("keypress", function (event) {
     li.appendChild(deleteBtn);
     list.appendChild(li); // ul vitra list hai halyau
 
+
     inp.value = ""; // yo lay chai input ma value hanya paxi enter garyako value lai chear garnya vayo
+
    
 }
+
+
+
 inp.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         attendance();
@@ -59,9 +71,16 @@ inp.addEventListener("keypress", function (event) {
 save.addEventListener("click", function () {
     let items = document.querySelectorAll(".studentlist li span");
     let content = "";
+    
+    if(items.length===0)
+    {
+        alert("Please Enter Full Name Or Roll No");
+    }
+else{
+
 
     items.forEach(item => {
-        content += item.textContent + "\n";
+        content += item.textContent + "\n\n";
     });
 
     let blob = new Blob([content], { type: "text/plain" });
@@ -69,4 +88,17 @@ save.addEventListener("click", function () {
     a.href = URL.createObjectURL(blob);
     a.download = "attendance.txt";
     a.click();
+}
 });
+remove.addEventListener("click",function(){
+   
+let all =document.querySelector(".studentlist");
+all.innerHTML=""
+
+
+
+})
+
+    
+
+
